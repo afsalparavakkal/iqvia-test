f41675c69a5a8b3b8d26f3f5f37279733446c56b
mparavak <mparavak@cisco.com>
2022-06-30 21:54:22 +0530

    Updated the logic

--- code_refactoring.py
+++ code_refactoring.py
@@ -1,25 +1,16 @@
-
 import os
 from time import time
 from random import randint
-
 cur_dir = os.path.dirname(__file__)
 file_name = os.path.join(cur_dir, 'random_numbers.txt')
-
 a = time()
-random_number = ''
 
-f = open(file_name, 'w')
-
-for i in range(5000000):
-    random_number = str(randint(0, 9999))
-    f.write(random_number+'\n')
-
-f.close()
-
-# with open(file_name, 'w') as f:
-#     f.write(random_number)
+random_numbers = []
+with open(file_name, 'w') as f:
+    for i in range(5000000):
+        random_numbers.append(str(randint(0, 9999)))
+        
+    f.writelines(random_numbers)
 
 b = time()
 print('Execution time: {}'.format(b - a))
-
